53ec2493ab877a4c2df1261f4ef9d6c16ed96811
izzy <isamede3@gmail.com>
2025-07-24 09:04:15 +0100

    add bat and eza

--- user/packages/packages.nix
+++ user/packages/packages.nix
@@ -2,5 +2,6 @@
 {
   home.packages = with pkgs; [
     btop
+    eza
   ];
 }
--- user/shell.nix
+++ user/shell.nix
@@ -3,19 +3,32 @@ let
   aliases = {
     nrs = "sudo nixos-rebuild switch --flake ~/.dotfiles";
     hms = "home-manager switch --flake ~/.dotfiles";
+
     q = "exit";
+
     gst = "git status";
     gaa = "git add --all";
     gcmsg = "git commit -m";
     gpush = "git push";
     gpull = "git pull";
+
+    ls = "eza --color=always --icons=always";
+    ll = "eza --color=always --icons=always -l";
+    la = "eza --color=always --icons=always -a";
+    lla = "eza --color=always --icons=always -la";
+
+    cat = "bat --color=always";
   };
 in
 {
   programs.zsh = {
     enable = true;
     shellAliases = aliases;
-    initContent = ''ZLE_PROMPT_INDENT=0'';
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = "Nord";
   };
 
   programs.starship = {
